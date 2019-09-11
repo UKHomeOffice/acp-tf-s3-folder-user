0685ecdb30e35e9f21e9bd18a9f158c687e475db
Adam Smith <adam.smith@digital.homeoffice.gov.uk>
2019-09-11 10:11:02 +0100

    Create initial module

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "bucket_arn" {
+  description = ""
+}
+
+variable "bucket_name" {
+  description = ""
+}
+
+variable "bucket_path" {
+  description = ""
+  default     = ""
+}
+
+variable "iam_user" {
+  description = ""
+}
+
+variable "kms_alias" {
+  description = ""
+}
